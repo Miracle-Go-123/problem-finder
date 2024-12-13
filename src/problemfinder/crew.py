62353b9375a1a4482417c1e065ef077b52e07c52
MiracleWorker <entsav.go@yahoo.com>
2024-12-13 09:55:06 -0600

    initial crew structure

---
+++ src/problemfinder/crew.py
@@ -0,0 +1,62 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task, before_kickoff
+import json
+from pathlib import Path
+
+# If you want to run a snippet of code before or after the crew starts, 
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+@CrewBase
+class ProblemFinder():
+	"""ProblemFinder crew"""
+
+	agents_config = 'config/agents.yaml'
+	tasks_config = 'config/tasks.yaml'
+
+	@before_kickoff
+	def prepare_data(self, inputs):
+		"""
+		Prepare chat log and uploaded documents data for the problem finder
+		"""
+		chat_path = Path("assets") / "chat.json"
+		docs_path = Path("assets") / "document.json"
+		
+		with open(chat_path, "r") as file:    
+			chat = json.load(file)
+		
+		with open(docs_path, "r") as file:    
+			uploaded_documents = json.load(file)
+		
+		modified_inputs = {
+			"chat": chat,
+			"documents": uploaded_documents,
+		}
+
+		return modified_inputs
+	
+	@agent
+	def problem_finder(self) -> Agent:
+		return Agent(
+			config=self.agents_config['problem_finder'],
+			verbose=True,
+			memory=True,
+		)
+	
+	@task
+	def problem_finder_task(self) -> Task:
+		config=self.tasks_config['problem_finder_task']
+		# config['output_json'] = outputPydantic
+		config['expected_output'] = "list"
+		return Task(
+			config=config,
+		)
+	
+	@crew
+	def crew(self) -> Crew:
+		return Crew(
+			agents=self.agents,
+			tasks=self.tasks,
+			process=Process.sequential,
+			verbose=False,
+		)
